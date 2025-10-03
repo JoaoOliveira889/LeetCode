2af705eb88869a7c2515630265fa5235667b6710
Joao Oliveira <joao8809@icloud.com>
2025-10-03 12:52:45 -0300

    add 48

---
+++ Medium/48.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        int size = matrix.Length;
+
+        for(int i = 0; i < size; i++){
+            for(int j = i; j< size; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i = 0; i < size; i++){
+            int start = 0;
+            int end = size -1;
+
+            while(start < end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                
+                start++;
+                end--;
+            }
+        }
+    }
+}
+
